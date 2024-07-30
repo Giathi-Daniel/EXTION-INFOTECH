ae23b2ce21e68e289263aa2c0184ae6a9ccb8f89
Daniel Giathi <blacksharkchi@proton.me>
2024-07-30 13:04:04 +0300

    refactor

--- 01-QuizApp/server.js
+++ 01-QuizApp/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3000;
+const port = 5000;
 
 const db = new sqlite3.Database('quiz.db');
 
