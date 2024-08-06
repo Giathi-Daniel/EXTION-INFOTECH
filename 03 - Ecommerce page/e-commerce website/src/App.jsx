d56f22c1e0e361ee36f1923d133e7ebf9a6b269d
Daniel Giathi <blacksharkchi@proton.me>
2024-08-06 19:46:10 +0300

    install react-router-dom and create default pages

---
+++ 03 - Ecommerce page/e-commerce website/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SignIn from './pages/SignIn/SignIn';
+import Header from './pages/Header/Header';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
