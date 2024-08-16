674ec89de3cba706f1c1c8d1372999479abb9426
Daniel Giathi <blacksharkchi@proton.me>
2024-08-16 06:46:10 +0300

    add express for backend

--- 04 - BLOG WEBSITE/client/src/pages/Login.js
+++ 04 - BLOG WEBSITE/client/src/pages/Login.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 const Login = () => {
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useStae('')
+
   return (
-    <form action="" className="login">
+    <form action="" className="login" onSubmit>
       <h1>Login</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input type="text" 
+      placeholder="username"
+      value={username}
+      onChange={ev => setUserName(ev.target.value)} 
+      />
+      <input type="password" 
+      placeholder="password"
+      value={password}
+      onChange={ev => setUserName(ev.target.value)} 
+       />
       <button>Login</button>
     </form>
   )
--- 04 - BLOG WEBSITE/client/src/pages/Register.js
+++ 04 - BLOG WEBSITE/client/src/pages/Register.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
-const register = () => {
+const Login = () => {
+  const [username, setUserName] = useState('')
+  const
+  
+  async function register(ev) {
+    ev.preventDefault()
+
+    await fetch('https://localhost:4000'), {
+      method: 'POST',
+      body: JSON.sptrigify({username, password}),
+      headers: {'Content-type ':''application/json '}
+  }
+
   return (
-    <form action="" className="register">
-      <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+    <form action="" className="login" onSubmit={register}>
+      <h1>Login</h1>
+      <input type="text" 
+      placeholder="username"
+      value={username}
+      onChange={ev => setUserName(ev.target.value)} 
+      />
+      <input type="password" 
+      placeholder="password"
+      value={password}
+      onChange={ev => setUserName(ev.target.value)} 
+       />
       <button>Login</button>
     </form>
   )
 }
 
-export default register
+export default Login
