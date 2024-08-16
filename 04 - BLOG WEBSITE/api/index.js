5a8aae32595fffd3e6d6b95b51dc8d51a59e51aa
Daniel Giathi <blacksharkchi@proton.me>
2024-08-16 10:25:24 +0300

    create post page

--- 04 - BLOG WEBSITE/api/index.js
+++ 04 - BLOG WEBSITE/api/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/user');  
+const Post = require('./models/post')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const multer = require('multer')
+const uploadMiddleware = multer({dest: 'uploads/'})
+const fs = require('fs')
 
 const app = express();
 
@@ -75,6 +79,25 @@ app.get('/profile', (req, res) => {
 // logout
 app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok')
+})
+
+// Create post
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const {originalname, path} = req.file
+    const parts = originalname.split('.')
+    const ext = parts[parts.length - 1]
+    const newPath = path+'.'+ext
+    fs.renameSync(path, newPath)
+
+    const {title,summary,content} = req.body
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover:newPath
+    })
+
+    res.json(postDoc)
 })
 
 app.listen(4000)
--- 04 - BLOG WEBSITE/client/src/App.js
+++ 04 - BLOG WEBSITE/client/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Layout from './component/Layout';
 import { UserContextProvider } from './UserContext';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route index element={<Home />}/>
       <Route path="/login" element={<Login />}/>
       <Route path="/register" element={<Register />}/>
+      <Route path="/create" element={<CreatePost />}/>
     </Routes>
     </UserContextProvider>
   );
